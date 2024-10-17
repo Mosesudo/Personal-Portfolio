0b3d8b13c35ec18d2b2e0bbb2ea333aec540cd00
MKLEO <mosesudosen89@gmail.com>
2024-10-17 13:22:33 +0100

    third commit

--- build/app.js
+++ build/app.js
@@ -1,9 +1,28 @@
 const button = document.getElementById("button");
 const navlinks = document.getElementById("nav-links");
-const bars = document.getElementById("#bars")
+const bars = document.getElementById("bars");
+const ball = document.getElementById('ball');
+const container = document.getElementById('container');
+const text = document.getElementById('text')
+const body = document.getElementById("body");
 button.addEventListener("click", () => {
     navlinks.classList.toggle("hidden");
 })
 navlinks.addEventListener("click", () => {
     navlinks.classList.add("hidden")
+})
+ball.addEventListener("click", () =>
+{  
+    if(!body.classList.contains('dark')) {
+    body.classList.add('dark')
+text.innerText = "Light Mode"
+ball.style.transform = 'translateX(19px)';
+ball.style.background = "white"
+    }
+    else {
+        body.classList.remove("dark");
+        text.innerText = "Dark Mode";
+        ball.style.transform = "translateX(2px)"
+        ball.style.background = "black"
+    }
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#08163b'
+      }
+    },
+  },
+  plugins: [],
+}
 
-   
